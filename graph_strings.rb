9473a7f50779cf6641c3304f526679fd15c41124
evanmce <ecm53@pitt.edu>
2018-12-02 16:12:35 -0500

    rubocopped

--- graph_strings.rb
+++ graph_strings.rb
@@ -1,35 +1,29 @@
-class String_Collector
-    attr_accessor :strings
+# Get them strings
+class StringCollector
+  attr_accessor :strings
 
-    def initialize
-        @strings = []
-    end
-    
-    def call (*string)
-        strings << []
-        puts string
-    end
+  def initialize
+    @strings = []
+  end
+
+  def call(*string)
+    strings << []
+    puts string
+  end
 end
 
 def graph_strings(in_graph, callback)
-    in_graph::nodes.map do |node|
-        callback.call(node::letter)
-        breath_first_search(node, node::letter, callback)
-    end
+  in_graph.nodes.map do |node|
+    callback.call(node.letter)
+    breath_first_search(node, node.letter, callback)
+  end
 end
 
 def breath_first_search(node, prefix, callback)
-    node::neighbors.each do |n| 
-        callback.call(prefix + n::letter)
-    end
-    node::neighbors.each do |n|
-        breath_first_search(n, prefix + n::letter, callback)
-    end
+  node.neighbors.each do |n|
+    callback.call(prefix + n.letter)
+  end
+  node.neighbors.each do |n|
+    breath_first_search(n, prefix + n.letter, callback)
+  end
 end
-
-
-
-
-
-
-
