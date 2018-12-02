5feabeed46e178d82c11a023ff40f3fbd2013f9d
evanmce <ecm53@pitt.edu>
2018-12-02 15:19:47 -0500

    gets node links

--- makegraph.rb
+++ makegraph.rb
@@ -4,7 +4,10 @@ def make_graph(arg)
   graph = Graph.new
   File.open(arg[0], 'r').each do |line|
     number, char = /(?<number>\d{1});(?<char>\w{1});/.match(line).captures
-    graph.nodes << Node.new(number, char)
+    node = Node.new(number, char)
+    line = line.sub(/(?<number>\d{1});(?<char>\w{1});/, '')
+    node.neighbors << line.scan(/(\d),*/)
+    graph.nodes << node
   end
 
   graph.display
