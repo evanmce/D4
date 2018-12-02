7c678a4b3a935cd02c6f296ecac76ea0cf23841d
Ryan Guild <ryan.guild@pyrantix.org>
2018-12-02 15:27:56 -0500

    graph of nodes

--- makegraph.rb
+++ makegraph.rb
@@ -2,12 +2,19 @@ require_relative 'graph.rb'
 
 def make_graph(arg)
   graph = Graph.new
+  size = `wc -l #{arg[0]}`.to_i
+  (1..size).forEach do |name|
+    graph.nodes << Node.new(name)
+  end
   File.open(arg[0], 'r').each do |line|
     number, char = /(?<number>\d{1});(?<char>\w{1});/.match(line).captures
-    node = Node.new(number, char)
+    graph.nodes[number - 1].set_letter(char)
     line = line.sub(/(?<number>\d{1});(?<char>\w{1});/, '')
-    node.neighbors << line.scan(/(\d),*/)
-    graph.nodes << node
+    nums = []
+    nums << line.scan(/(\d),*/)
+    nums.forEach do |neighbor|
+      graph.nodes[number - 1].neighbors << graph.nodes[neighbor.to_i - 1]
+    end
   end
 
   graph.display
