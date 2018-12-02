265855e6e3a2b0f948adc3f4caf6145707a5b9e9
evanmce <ecm53@pitt.edu>
2018-12-02 14:18:05 -0500

    displays graph

--- makegraph.rb
+++ makegraph.rb
@@ -4,8 +4,8 @@ def make_graph(arg)
   graph = Graph.new
   File.open(arg[0], 'r').each do |line|
     number, char = /(?<number>\d{1});(?<char>\w{1});/.match(line).captures
-    puts number
-    puts char
     graph.nodes << Node.new(number, char)
   end
+
+  graph.display
 end
