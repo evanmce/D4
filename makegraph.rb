1d04bc055ddbf6e49dbca65411207eca4167021e
evanmce <ecm53@pitt.edu>
2018-12-02 16:08:43 -0500

    rubocopped

--- makegraph.rb
+++ makegraph.rb
@@ -2,14 +2,14 @@ require_relative 'graph.rb'
 
 def make_graph(arg)
   graph = Graph.new
-  file = File.open(arg[0],"r").readlines
+  file = File.open(arg[0], 'r').readlines
   size = file.count
   (1..size).each do |name|
     graph.nodes << Node.new(name)
   end
   file.each do |line|
     number, char = /(?<number>\d{1});(?<char>\w{1});/.match(line).captures
-    graph.nodes[number.to_i - 1].set_letter(char)
+    graph.nodes[number.to_i - 1].letter = char
     line = line.sub(/(?<number>\d{1});(?<char>\w{1});/, '')
     nums = []
     nums << line.scan(/(\d),*/)
